86ce66b976350e59d3bacd4fe5868dd740dc317e
Michael Zelenyuk <zell.xp@gmail.com>
2015-05-03 17:02:18 +0300

    Register in Front End Views

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name chidoriApp
+ * @description
+ * # chidoriApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('chidoriApp', ['ui.router']);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chidoriApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chidoriApp
+ */
+angular.module('chidoriApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
